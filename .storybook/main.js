28936f06a42d45d92545c6979b9d9af70c772db4
Justin Mahar <contact@justinmahar.com>
2023-01-10 21:58:06 -0500

    Run updater

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,11 +6,17 @@ module.exports = {
     '../src/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [
+    // '@storybook/addon-essentials', // https://storybook.js.org/docs/react/essentials/introduction
     '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    // Support Markdown transclusion for README import - https://github.com/storybookjs/storybook/issues/7644
+    '@storybook/addon-viewport',
+    '@storybook/addon-controls',
+    '@storybook/addon-backgrounds',
+    '@storybook/addon-toolbars',
+    '@storybook/addon-measure',
+    '@storybook/addon-outline',
     {
       name: '@storybook/addon-docs',
+      // Support Markdown transclusion for README import - https://github.com/storybookjs/storybook/issues/7644
       options: { transcludeMarkdown: true },
     },
   ],
