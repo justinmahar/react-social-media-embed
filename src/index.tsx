8ce28d832e79183084b274b598f6190df4dd6373
Tim de Koning <tkoning@mediainfogroep.nl>
2023-01-12 12:36:32 +0100

    Export PinterestEmbed-component from main module

--- src/index.tsx
+++ src/index.tsx
@@ -4,5 +4,6 @@ export * from './components/embeds/LinkedInEmbed';
 export * from './components/embeds/TikTokEmbed';
 export * from './components/embeds/TwitterEmbed';
 export * from './components/embeds/YouTubeEmbed';
+export * from './components/embeds/PinterestEmbed';
 export * from './components/placeholder/PlaceholderEmbed';
 export * from './components/placeholder/parts/BorderSpinner';
